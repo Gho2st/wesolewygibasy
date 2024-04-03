fe4e3ca8813750139e052ae526e7afd652ba53f6
Dominik <dominik.jojczyk@gmail.com>
2024-04-03 22:14:30 +0200

    test new font

--- app/layout.js
+++ app/layout.js
@@ -1,9 +1,9 @@
-import { Poppins } from "next/font/google";
+import { Montserrat } from "next/font/google";
 import "./globals.css";
 
-const poppins = Poppins({
+const montserrat = Montserrat({
   subsets: ["latin"],
-  weight: ["100", "200", "300", "400", "500"],
+  weight: ["100", "200", "300", "400", "500", "600", "700"],
 });
 
 export const metadata = {
@@ -14,7 +14,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={poppins.className}>{children}</body>
+      <body className={montserrat.className}>{children}</body>
     </html>
   );
 }
