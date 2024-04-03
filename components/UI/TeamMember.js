2c9764daee3edca728eeea806cc8255890bd6878
Dominik <dominik.jojczyk@gmail.com>
2024-04-03 14:00:03 +0200

    0.2

--- components/UI/TeamMember.js
+++ components/UI/TeamMember.js
@@ -3,15 +3,15 @@ import Image from "next/image";
 
 export default function TeamMember(props) {
   return (
-    <div className={classes.container}>
-      <div className={classes.imageContainer}>
-        <Image src={props.src} width={230} height={230} alt="" />
+    <div className={classes.outside}>
+      <div className={classes.container}>
+        <div className={classes.imageContainer}>
+          <Image src={props.src} width={230} height={230} alt="" />
+        </div>
+        <h4>{props.name}</h4>
+        <h5>{props.role}</h5>
+        <p>{props.quote}</p>
       </div>
-      <h4>{props.name}</h4>
-      <h5>{props.role}</h5>
-      <p>
-        {props.quote}
-      </p>
     </div>
   );
 }
