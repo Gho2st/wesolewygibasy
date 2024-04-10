a82780ae2444a5b92d9b72fe7c945e83a6415c6d
Dominik <dominik.jojczyk@gmail.com>
2024-04-10 22:29:21 +0200

    sitemap added

---
+++ app/sitemap.js
@@ -0,0 +1,77 @@
+export default function sitemap() {
+    return [
+      {
+        url: 'https://wesolewygibasy.pl',
+        lastModified: new Date(),
+        changeFrequency: 'weekly',
+        priority: 1,
+      },
+      {
+        url: 'https://wesolewygibasy.pl/zapisy',
+        lastModified: new Date(),
+        changeFrequency: 'weekly',
+        priority: 0.8,
+      },
+      {
+        url: 'https://wesolewygibasy.pl/cennik',
+        lastModified: new Date(),
+        changeFrequency: 'weekly',
+        priority: 0.8,
+      },
+      {
+        url: 'https://wesolewygibasy.pl/plan-dnia',
+        lastModified: new Date(),
+        changeFrequency: 'weekly',
+        priority: 0.8,
+      },
+      {
+        url: 'https://wesolewygibasy.pl/informacje',
+        lastModified: new Date(),
+        changeFrequency: 'weekly',
+        priority: 0.8,
+      },
+      {
+        url: 'https://wesolewygibasy.pl/o-nas',
+        lastModified: new Date(),
+        changeFrequency: 'weekly',
+        priority: 0.8,
+      },
+      {
+        url: 'https://wesolewygibasy.pl/galeria',
+        lastModified: new Date(),
+        changeFrequency: 'weekly',
+        priority: 0.8,
+      },
+      {
+        url: 'https://wesolewygibasy.pl/zlobki',
+        lastModified: new Date(),
+        changeFrequency: 'weekly',
+        priority: 0.8,
+      },
+      {
+        url: 'https://wesolewygibasy.pl/zlobki/glogera',
+        lastModified: new Date(),
+        changeFrequency: 'weekly',
+        priority: 0.8,
+      },
+      {
+        url: 'https://wesolewygibasy.pl/zlobki/vetulaniego',
+        lastModified: new Date(),
+        changeFrequency: 'weekly',
+        priority: 0.8,
+      },
+      {
+        url: 'https://wesolewygibasy.pl/zlobki/sliczna',
+        lastModified: new Date(),
+        changeFrequency: 'weekly',
+        priority: 0.8,
+      },
+      {
+        url: 'https://wesolewygibasy.pl/zlobki/stanczyka',
+        lastModified: new Date(),
+        changeFrequency: 'weekly',
+        priority: 0.8,
+      },
+      
+    ]
+  }
