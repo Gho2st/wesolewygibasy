5173e0b6646096f0d55c8460379c5bf9df3c14cb
Dominik <dominik.jojczyk@gmail.com>
2024-08-06 17:05:57 +0200

    1.3

--- app/informacje-dla-rodzicow/page.js
+++ app/informacje-dla-rodzicow/page.js
@@ -42,18 +42,20 @@ export default function informacje() {
               Pobierz
             </a>
           </div>
-          <h2 className={classes.marginTop}> Klauzula prywatności</h2>
-          <p>
-            Od dnia 25 maja 2018 roku obowiązują nowe zasady ochrony danych
-            osobowych, które wynikają z postanowień RODO. RODO to Rozporządzenie
-            o Ochronie Danych Osobowych – pełna nazwa to: Rozporządzenie
-            Parlamentu Europejskiego i Rady (UE) 2016/679 z dnia 27 kwietnia
-            2016 r. w sprawie ochrony osób fizycznych w związku z przetwarzaniem
-            danych osobowych i w sprawie swobodnego przepływu takich danych oraz
-            uchylenia dyrektywy 95/46/WE. W poniższym pliku zawarte są
-            informacje o przetwarzaniu danych osobowych w Wesołych Wygibasach.
-          </p>
+
           <div className={classes.button}>
+            <h2> Klauzula prywatności</h2>
+            <p>
+              Od dnia 25 maja 2018 roku obowiązują nowe zasady ochrony danych
+              osobowych, które wynikają z postanowień RODO. RODO to
+              Rozporządzenie o Ochronie Danych Osobowych – pełna nazwa to:
+              Rozporządzenie Parlamentu Europejskiego i Rady (UE) 2016/679 z
+              dnia 27 kwietnia 2016 r. w sprawie ochrony osób fizycznych w
+              związku z przetwarzaniem danych osobowych i w sprawie swobodnego
+              przepływu takich danych oraz uchylenia dyrektywy 95/46/WE. W
+              poniższym pliku zawarte są informacje o przetwarzaniu danych
+              osobowych w Wesołych Wygibasach.
+            </p>
             <a href="/RODO-klauzula-informacyjna-copy.jpg" download>
               Pobierz klauzulę
             </a>
