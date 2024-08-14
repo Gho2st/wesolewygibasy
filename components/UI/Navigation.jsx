f2d8a0ddb3143747a36a4cea4760e5bf0d41b9d6
Dominik <dominik.jojczyk@gmail.com>
2024-08-14 10:04:42 +0200

    some visual changes

--- components/UI/Navigation.jsx
+++ components/UI/Navigation.jsx
@@ -22,7 +22,7 @@ export default function Navigation() {
     <>
       <div className={classes.logoContainer}>
         <Link className={classes.logo} href={"/"}>
-          <Image src={"/others/logo.png"} width={140} height={70}></Image>
+          <Image src={"/others/logo.png"} width={130} height={65}></Image>
         </Link>
       </div>
       <div className={classes.navigation}>
