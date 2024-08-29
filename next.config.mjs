90c116db6e0c5bffa699c6898b3c2db0634c9cd0
Dominik <dominik.jojczyk@gmail.com>
2024-08-29 12:01:51 +0200

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["hukimuki.s3.eu-central-1.amazonaws.com"],
     unoptimized: true,
   },
   reactStrictMode: false,
