08707a830fdaef37d1d6f6c28b9f236181d6124e
Dominik <dominik.jojczyk@gmail.com>
2024-09-15 17:52:51 +0200

    1.3

--- components/homepage/Baner.jsx
+++ components/homepage/Baner.jsx
@@ -2,35 +2,15 @@
 import Link from "next/link";
 import Button from "../UI/Button";
 import classes from "./Baner.module.css";
-import { useRef } from "react";
-import { useInView, motion, delay } from "framer-motion";
-import Image from "next/image";
 
 export default function Baner() {
-  const text = "Najlepszy Żłobek w Krakowie - Wesołe Wygibasy".split(" ");
-  const skillRef = useRef();
-  const isSkillRefinView = useInView(skillRef);
   return (
     <article>
       <div className={classes.banerContainer}>
-        <div className={classes.textContainer} ref={skillRef}>
+        <div className={classes.textContainer}>
           <div className={classes.left}>
             <div>
-              <h1>
-                {text.map((el, i) => (
-                  <motion.span
-                    initial={{ opacity: 0 }}
-                    animate={{ opacity: 1 }}
-                    transition={{
-                      duration: 0.5,
-                      delay: i / 10,
-                    }}
-                    key={i}
-                  >
-                    {el}{" "}
-                  </motion.span>
-                ))}
-              </h1>
+              <h1>Najlepszy Żłobek w Krakowie - Wesołe Wygibasy</h1>
               <p>
                 Wesołe Wygibasy to żłobek w Krakowie, który powstał z pasji,
                 troski i miłości do dzieci. Znajdujemy się w dogodnych
