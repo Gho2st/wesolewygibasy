407e8dfa5dc4b9b6a9927ca22d244a0daf306826
Dominik <dominik.jojczyk@gmail.com>
2024-12-27 21:15:51 +0100

    1.2

--- components/Gallery/ImageItem.js
+++ components/Gallery/ImageItem.js
@@ -4,14 +4,20 @@ import Image from "next/image";
 export default function ImagesItem(props) {
   return (
     <div className={classes.innerContainer} onClick={props.onClick}>
-      <Image
-        src={props.src}
-        width={100}
-        height={100}
-        layout="responsive"
-        className={classes.image}
-        alt={props.alt}
-      />
+      {props.src ? (
+        <Image
+          src={props.src}
+          width={100}
+          height={100}
+          layout="responsive"
+          className={classes.image}
+          alt={props.alt || "Image"}
+        />
+      ) : (
+        <div className={classes.placeholder}>
+          <p>Brak obrazu</p>
+        </div>
+      )}
       <p>{props.text}</p>
     </div>
   );
