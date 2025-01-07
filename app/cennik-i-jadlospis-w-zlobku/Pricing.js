9ee01fff6e8a586685de5cbf4e9c8d318d9bdbd2
Dominik <dominik.jojczyk@gmail.com>
2025-01-07 10:28:56 +0100

    2.1

--- app/cennik-i-jadlospis-w-zlobku/Pricing.js
+++ app/cennik-i-jadlospis-w-zlobku/Pricing.js
@@ -14,48 +14,52 @@ export default function Pricing() {
 
   return (
     <>
-      <main className={classes.mainContainer}>
-        <Header text="Cennik - Żłobek Wesołe Wygibasy w Krakowie" />
-        <div className={classes.description}>
-          <p>
-            Zapraszamy do zapoznania się z naszym cennikiem dla żłobka Wesołe
-            Wygibasy w Krakowie. Znajdą Państwo tutaj szczegółowe informacje na
-            temat pełnej kwoty abonamentu, liczby dni roboczych, miesięcznej
-            dotacji oraz jadłospisu. Nasz żłobek w Krakowie oferuje
-            konkurencyjne ceny, które obejmują wszystko, czego potrzebują
-            Państwa dzieci do bezpiecznej i kreatywnej nauki oraz zabawy.
-            Jesteśmy dumni, że możemy zapewnić Państwa maluchom opiekę na
-            najwyższym poziomie w przystępnych cenach. Dbamy o to, aby każdy
-            dzień w naszym żłobku był pełen radości, nauki i zdrowego rozwoju.
-          </p>
-          <Link href={"/plan-dnia-w-zlobku"}>
-            Zobacz jak wygląda plan dnia w naszym żłobku
-          </Link>
-          <p>
-            W dniu zawarcia umowy Rodzic/Opiekun Prawny zobowiązany jest do
-            wniesienia opłaty wpisowej (administracyjnej) w wysokości 600,00 zł
-            (słownie: sześćset złotych). Opłata ta obejmuje:
-          </p>
-          <ul>
-            <li>
-              koszt zakupu podstawowych pomocy dydaktycznych, edukacyjnych i
-              plastycznych
-            </li>
-            <li>
-              koszt zakupu niezbędnych przedmiotów osobistych dla Dziecka na
-              czas pobytu w żłobku (mokre chusteczki, ręcznik papierowy, nocnik
-              itp.
-            </li>
-            <li>
-              koszt zakupu prezentów okolicznościowych (Mikołajki, Dzień
-              Dziecka, Urodziny Dziecka).
-            </li>
-            <li>Zakup pościeli dla dziecka.</li>
-          </ul>
-          <p>
-            W przypadku dodatkowych pytań lub wątpliwości zapraszamy do kontaktu
-            z naszą placówką – chętnie udzielimy wszelkich informacji.
-          </p>
+      <main>
+        <div className={classes.textContainer}>
+          <Header text="Cennik - Żłobek Wesołe Wygibasy w Krakowie" />
+          <div className={classes.description}>
+            <p>
+              Zapraszamy do zapoznania się z naszym cennikiem dla żłobka Wesołe
+              Wygibasy w Krakowie. Znajdą Państwo tutaj szczegółowe informacje
+              na temat pełnej kwoty abonamentu, liczby dni roboczych,
+              miesięcznej dotacji oraz jadłospisu. Nasz żłobek w Krakowie
+              oferuje konkurencyjne ceny, które obejmują wszystko, czego
+              potrzebują Państwa dzieci do bezpiecznej i kreatywnej nauki oraz
+              zabawy. Jesteśmy dumni, że możemy zapewnić Państwa maluchom opiekę
+              na najwyższym poziomie w przystępnych cenach. Dbamy o to, aby
+              każdy dzień w naszym żłobku był pełen radości, nauki i zdrowego
+              rozwoju.
+            </p>
+            <Link href={"/plan-dnia-w-zlobku"}>
+              Zobacz jak wygląda plan dnia w naszym żłobku
+            </Link>
+            <p>
+              W dniu zawarcia umowy Rodzic/Opiekun Prawny zobowiązany jest do
+              wniesienia opłaty wpisowej (administracyjnej) w wysokości 600,00
+              zł (słownie: sześćset złotych). Opłata ta obejmuje:
+            </p>
+            <ul>
+              <li>
+                koszt zakupu podstawowych pomocy dydaktycznych, edukacyjnych i
+                plastycznych
+              </li>
+              <li>
+                koszt zakupu niezbędnych przedmiotów osobistych dla Dziecka na
+                czas pobytu w żłobku (mokre chusteczki, ręcznik papierowy,
+                nocnik itp.
+              </li>
+              <li>
+                koszt zakupu prezentów okolicznościowych (Mikołajki, Dzień
+                Dziecka, Urodziny Dziecka).
+              </li>
+              <li>Zakup pościeli dla dziecka.</li>
+            </ul>
+            <p>
+              W przypadku dodatkowych pytań lub wątpliwości zapraszamy do
+              kontaktu z naszą placówką – chętnie udzielimy wszelkich
+              informacji.
+            </p>
+          </div>
         </div>
         <div className={classes.pricingContainer}>
           <h2
