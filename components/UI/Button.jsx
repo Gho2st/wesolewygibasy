f64439bf3f35d840de07c818913684d8a6b52cc2
Dominik <dominik.jojczyk@gmail.com>
2025-03-18 21:45:44 +0100

    2

--- components/UI/Button.jsx
+++ components/UI/Button.jsx
@@ -5,17 +5,17 @@ import Link from "next/link";
 
 export default function Button(props) {
   return (
-    <motion.div
-      className={classes.buttonContainer}
-      whileHover={{ scale: 1.08 }}
-      whileTap={{ scale: 0.9 }}
+    <Link
+      href={props.href}
+      style={{ fontSize: props.fontSize, background: props.background }}
     >
-      <Link
-        href={props.href}
-        style={{ fontSize: props.fontSize, background: props.background }}
+      <motion.div
+        className={classes.buttonContainer}
+        whileHover={{ scale: 1.08 }}
+        whileTap={{ scale: 0.9 }}
       >
         {props.text}
-      </Link>
-    </motion.div>
+      </motion.div>
+    </Link>
   );
 }
--- components/UI/Button2.jsx
+++ components/UI/Button2.jsx
@@ -5,13 +5,15 @@ import { motion } from "framer-motion";
 
 export default function Button2(props) {
   return (
-    <motion.div
-      className={classes.buttonContainer}
-      whileHover={{ scale: 1.08 }}
-      whileTap={{ scale: 0.9 }}
-      style={{ background: props.background, color: props.color }}
-    >
-      <Link href={props.href}>{props.text}</Link>
-    </motion.div>
+    <Link href={props.href}>
+      <motion.div
+        className={classes.buttonContainer}
+        whileHover={{ scale: 1.08 }}
+        whileTap={{ scale: 0.9 }}
+        style={{ background: props.background, color: props.color }}
+      >
+        {props.text}
+      </motion.div>
+    </Link>
   );
 }
