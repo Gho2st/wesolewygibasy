2e954bbe74f1c925bc917b90a0bc5c678d38c5fb
Dominik <dominik.jojczyk@gmail.com>
2025-03-23 14:22:43 +0100

    1

--- components/UI/Footer.jsx
+++ components/UI/Footer.jsx
@@ -7,13 +7,11 @@ import { FaMessage } from "react-icons/fa6";
 import { FaFacebook } from "react-icons/fa";
 import { FaInstagram } from "react-icons/fa";
 import Numbers from "./Numbers";
-import { FaArrowUp } from "react-icons/fa";
 
 export default function Footer() {
   const currentYear = new Date().getFullYear();
 
   const scrollToTop = () => {
-    console.log("Kliknięto strzałkę, przewijam w górę");
     window.scrollTo({
       top: 0,
       behavior: "smooth",
@@ -21,90 +19,111 @@ export default function Footer() {
   };
 
   return (
-    <footer>
-      <div className={classes.container}>
-        <div className={classes.footerTopContainer}>
-          <div className={classes.text}>
-            <h2>
-              <span>Wesołe Wygibasy</span> są wpisane do rejestru krakowskich
-              złobków i klubów dziecięcych.
-            </h2>
-            <p>
-              Oznacza to, ze jesteśmy miejscem bezpiecznym, przyjaznym dzieciom
-              i rodzicom!
-            </p>
-            <div className={classes.line}></div>
+    <>
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+        <path
+          fill="#4a71ff"
+          fill-opacity="1"
+          d="M0,192L80,176C160,160,320,128,480,133.3C640,139,800,181,960,181.3C1120,181,1280,139,1360,117.3L1440,96L1440,320L1360,320C1280,320,1120,320,960,320C800,320,640,320,480,320C320,320,160,320,80,320L0,320Z"
+        ></path>
+      </svg>
+      <footer className={classes.container}>
+        <div className="">
+          <div className={classes.topContainer}>
+            <div className={classes.textContainer}>
+              <h2>
+                <span>Wesołe Wygibasy</span> są wpisane do rejestru krakowskich
+                złobków i klubów dziecięcych
+              </h2>
+              <p>
+                Oznacza to, że jesteśmy miejscem bezpiecznym oraz przyjaznym
+                dzieciom i rodzicom!
+              </p>
+              <div className={classes.line}></div>
+            </div>
+            <div className={classes.numbers}>
+              <Numbers />
+            </div>
           </div>
-          <div className={classes.numbersContainer}>
-            <Numbers />
-          </div>
-        </div>
-        <div className={classes.footerInfoContainer}>
-          <div>
-            <h3>Godziny otwarcia</h3>
-            <ul>
-              <li>Pon - Pt </li>
-              <li>Vetulaniego - 7:00 - 17:00 </li>
-              <li>Glogera - 6:30 - 17:00 </li>
-              <li>Stańczyka - 6:30 - 17:30 </li>
-              <li>Śliczna - 7:00 - 17:30 </li>
-              <li>Sob: nieczynne</li>
-              <li>Ndz: nieczynne</li>
-            </ul>
-          </div>
-          <div>
-            <h3>Kontakt z Nami i Zapisy</h3>
-            <ul className={classes.contactInfo}>
-              <li className={classes.phone}>
-                <Link href={"tel:+48697560022"}>
-                  <FaPhone />
-                  697 560 022
-                </Link>
-              </li>
-              <li>
-                <MdEmail /> wesolewygibasy@onet.pl
-              </li>
-              <li className={classes.form}>
-                <Link href="/zapisy">
-                  <div className={classes.formContainer}>
-                    <div className={classes.icon}>
+          <div className={classes.footerInfoContainer}>
+            <div className={classes.hoursContainer}>
+              <h3>Godziny otwarcia</h3>
+              <ul>
+                <li>
+                  <span>Pon - Pt</span>{" "}
+                </li>
+                <li>Vetulaniego - 7:00 - 17:00 </li>
+                <li>Glogera - 6:30 - 17:00 </li>
+                <li>Stańczyka - 6:30 - 17:30 </li>
+                <li>Śliczna - 7:00 - 17:30 </li>
+                <li>
+                  <span>Sob:</span> nieczynne
+                </li>
+                <li>
+                  <span>Ndz:</span> nieczynne
+                </li>
+              </ul>
+            </div>
+            <div>
+              <h3>Kontakt z Nami i Zapisy</h3>
+              <ul className={classes.contactInfo}>
+                <li>
+                  <Link href={"tel:+48697560022"}>
+                    <div className={classes.iconContainer}>
+                      <FaPhone />
+                    </div>
+                    <div className={classes.linkContainer}>+48 697 560 022</div>
+                  </Link>
+                </li>
+                <li>
+                  <Link href="mailto:wesolewygibasy@onet.pl">
+                    <div className={classes.iconContainer}>
+                      <MdEmail />
+                    </div>
+                    <div className={classes.linkContainer}>
+                      wesolewygibasy@onet.pl
+                    </div>
+                  </Link>
+                </li>
+                <li>
+                  <Link href="/zapisy">
+                    <div className={classes.iconContainer}>
                       <FaMessage />
                     </div>
-                    <div>Formularz kontaktowy</div>
-                  </div>
+                    <div className={classes.linkContainer}>
+                      Formularz kontaktowy
+                    </div>
+                  </Link>
+                </li>
+              </ul>
+            </div>
+            <div className={classes.socialmedia}>
+              <h3>Social media</h3>
+              <div className={classes.socialContainer}>
+                <Link
+                  href="https://www.facebook.com/wesolewygibasy"
+                  aria-label="Poczytaj więcej na naszym facebooku Wesołe Wygibasy"
+                >
+                  <FaFacebook className={classes.facebook} />
                 </Link>
-              </li>
-            </ul>
+                <Link
+                  href="https://www.instagram.com/wesolewygibasy"
+                  aria-label="Poczytaj więcej na naszym instagramie Wesołe Wygibasy"
+                >
+                  <FaInstagram className={classes.instagram} />
+                </Link>
+              </div>
+            </div>
           </div>
-          <div className={classes.socialmedia}>
-            <h3>Social media</h3>
-            <Link
-              href="https://www.facebook.com/wesolewygibasy"
-              aria-label="Poczytaj więcej na naszym facebooku Wesołe Wygibasy"
-            >
-              <FaFacebook className={classes.facebook} />
-            </Link>
-            <Link
-              href="https://www.instagram.com/wesolewygibasy"
-              aria-label="Poczytaj więcej na naszym instagramie Wesołe Wygibasy"
-            >
-              <FaInstagram className={classes.instagram} />
-            </Link>
-          </div>
-        </div>
-        <div className={classes.footerDownContainer}>
-          <p className={classes.description}>
-            <span className={classes.copy}>&copy;</span> {currentYear} Wesołe
-            Wygibasy | Projekt i realizacja:
-            <span className={classes.FooterSpan}>
+          <div className={classes.footerDownContainer}>
+            <p className={classes.description}>
+              <span className={classes.copy}>&copy;</span> {currentYear} Wesołe
+              Wygibasy | Projekt i realizacja:
               <Link href={"https://www.domiweb.pl/"}> Domiweb </Link>
-            </span>
-          </p>
-          <span onClick={scrollToTop} className={classes.arrow}>
-            <FaArrowUp />
-          </span>
+            </p>
+          </div>
         </div>
-      </div>
-    </footer>
+      </footer>
+    </>
   );
 }
--- components/UI/Numbers.jsx
+++ components/UI/Numbers.jsx
@@ -10,76 +10,78 @@ export default function Numbers() {
 
   return (
     <>
-      <div className={classes.numbers}>
-        <p
-          onClick={() => {
-            setIsKlubMalucha(!isKlubMalucha);
-            setIsGlogera(false);
-            setIsStanczyka(false);
-            setIsSliczna(false);
-          }}
-        >
-          1
-        </p>
-        <p
-          onClick={() => {
-            setIsGlogera(!isGlogera);
-            setIsKlubMalucha(false);
-            setIsStanczyka(false);
-            setIsSliczna(false);
-          }}
-        >
-          2
-        </p>
-        <p
-          onClick={() => {
-            setIsStanczyka(!isStanczyka);
-            setIsGlogera(false);
-            setIsSliczna(false);
-            setIsKlubMalucha(false);
-          }}
-        >
-          3
-        </p>
-        <p
-          onClick={() => {
-            setIsSliczna(!isSliczna);
-            setIsGlogera(false);
-            setIsKlubMalucha(false);
-            setIsStanczyka(false);
-          }}
-        >
-          4
-        </p>
+      <div className={classes.numbersContainer}>
+        <div className={classes.numbers}>
+          <span
+            onClick={() => {
+              setIsKlubMalucha(!isKlubMalucha);
+              setIsGlogera(false);
+              setIsStanczyka(false);
+              setIsSliczna(false);
+            }}
+          >
+            1
+          </span>
+          <span
+            onClick={() => {
+              setIsGlogera(!isGlogera);
+              setIsKlubMalucha(false);
+              setIsStanczyka(false);
+              setIsSliczna(false);
+            }}
+          >
+            2
+          </span>
+          <span
+            onClick={() => {
+              setIsStanczyka(!isStanczyka);
+              setIsGlogera(false);
+              setIsSliczna(false);
+              setIsKlubMalucha(false);
+            }}
+          >
+            3
+          </span>
+          <span
+            onClick={() => {
+              setIsSliczna(!isSliczna);
+              setIsGlogera(false);
+              setIsKlubMalucha(false);
+              setIsStanczyka(false);
+            }}
+          >
+            4
+          </span>
+        </div>
+        {isKlubMalucha && (
+          <div className={classes.itemContainer}>
+            <h3 className="">Klub Malucha ul. Vetulaniego</h3>
+            <p>Nasz numer to:</p>
+            <p className={classes.number}>7330.2.16.2017</p>
+          </div>
+        )}
+        {isGlogera && (
+          <div className={classes.itemContainer}>
+            <h3>Niepubliczny Żłobek ul. Glogera</h3>
+            <p>Nasz numer to:</p>
+            <p className={classes.number}>7330.2.16.2017</p>
+          </div>
+        )}
+        {isStanczyka && (
+          <div className={classes.itemContainer}>
+            <h3>Niepubliczny Żłobek ul. Stańczyka</h3>
+            <p>Nasz numer to:</p>
+            <p className={classes.number}>7330.2.7.2023</p>
+          </div>
+        )}
+        {isSliczna && (
+          <div className={classes.itemContainer}>
+            <h3>Niepubliczny Żłobek ul. Śliczna</h3>
+            <p>Nasz numer to:</p>
+            <p className={classes.number}>7330.1.15.2024</p>
+          </div>
+        )}
       </div>
-      {isKlubMalucha && (
-        <div className={classes.itemContainer}>
-          <h3>Klub Malucha</h3>
-          <p>Nasz numer to:</p>
-          <p className={classes.number}>7330.2.16.2017</p>
-        </div>
-      )}
-      {isGlogera && (
-        <div className={classes.itemContainer}>
-          <h3>Niepubliczny Żłobek ul. Glogera</h3>
-          <p>Nasz numer to:</p>
-          <p className={classes.number}>7330.2.16.2017</p>
-        </div>
-      )}
-      {isStanczyka && (
-        <div className={classes.itemContainer}>
-          <h3>Niepubliczny Żłobek ul. Stańczyka</h3>
-          <p>Nasz numer to:</p>
-          <p className={classes.number}>7330.2.7.2023</p>
-        </div>
-      )}
-      {isSliczna && (
-        <div className={classes.itemContainer}>
-          <h3>Niepubliczny Żłobek ul. Śliczna</h3>
-          <p>Nasz numer to:</p>
-          <p className={classes.number}>7330.1.15.2024</p>
-        </div>
-      )}
     </>
   );
 }
