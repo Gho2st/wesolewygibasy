f7229ec2b0e8444c28e1d707088a733a6b6f2e9d
Dominik <dominik.jojczyk@gmail.com>
2025-09-02 08:30:10 +0200

    Update Button.jsx

--- components/UI/Button.jsx
+++ components/UI/Button.jsx
@@ -2,10 +2,11 @@
 import { motion } from "framer-motion";
 import Link from "next/link";
 
-export default function Button({ href, text }) {
+export default function Button({ href, text, onClick }) {
   return (
     <Link href={href}>
       <motion.div
+        onClick={onClick}
         whileHover={{ scale: 1.05 }}
         whileTap={{ scale: 0.9 }}
         className="bg-[#fa7070] text-white px-6 py-2 rounded-xl text-lg font-bold shadow-md mr-2 whitespace-nowrap min-w-fit transition-all duration-200 ease-in-out
