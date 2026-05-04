9aaf780ee00f148bddfe3b7e3fa9241138fd6941
Dominik <dominik.jojczyk@gmail.com>
2026-05-04 16:51:45 +0200

    Update route.js

--- app/api/sendEmail/route.js
+++ app/api/sendEmail/route.js
@@ -123,8 +123,8 @@ export async function POST(request) {
     const mailOptions = {
       from: process.env.NODEMAILER_EMAIL,
 
-      // to: "wesolewygibasy@onet.pl",
-      to: "dominikjojczyk@gmail.com",
+      to: "wesolewygibasy@onet.pl",
+      // to: "dominikjojczyk@gmail.com",
       subject: "Email ze strony Wesołe Wygibasy od klienta",
       html: createEmailTemplate(fields),
     };
